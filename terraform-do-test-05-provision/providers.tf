f0ed94272518062257c6fa22d989949937a28995
Egor Petrov <egor.petrov@powercom.co.il>
2024-04-03 11:19:19 +0300

    add provisioning

---
+++ terraform-do-test-05-provision/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "~> 1.5.7"
+
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.36.0"
+    }
+
+    circleci = {
+      source  = "kelvintaywl/circleci"
+      version = "1.0.1"
+    }
+
+  }
+}
+
+provider "digitalocean" {}
---
+++ terraform-do-test-05-provision/variables_ci.tf
@@ -0,0 +1,9 @@
+variable "digitalocean_registry_access_token" {
+  description = "DigitalOcean registry access token"
+  type = string
+}
+
+variable "digitalocean_registry_username" {
+  description = "DigitalOceanregistry username"
+  type = string
+}
