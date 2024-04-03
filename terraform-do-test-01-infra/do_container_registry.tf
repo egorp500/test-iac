1b7a25a4251d82bac42952b8c25e9b970394979f
Egor Petrov <egor.petrov@powercom.co.il>
2024-04-03 11:18:27 +0300

    add outputs (registry_name/registry_url)

--- terraform-do-test-01-infra/do_container_registry.tf
+++ terraform-do-test-01-infra/do_container_registry.tf
@@ -2,8 +2,16 @@ resource "random_id" "docker_registry_id" {
   byte_length = 4
 }
 
-resource "digitalocean_container_registry" "do_docker_container_egistry" {
+resource "digitalocean_container_registry" "do_docker_container_registry" {
   name                   = "dockerregistry${random_id.docker_registry_id.hex}"
   subscription_tier_slug = "starter"
   region                 = var.do_region_slug_prefix
+}
+
+output "do_docker_container_registry_name" {
+  value = digitalocean_container_registry.do_docker_container_registry.name
+}
+
+output "do_docker_container_registry_url" {
+  value = digitalocean_container_registry.do_docker_container_registry.server_url
 }
